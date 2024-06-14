08945313a7e1254c99813d13695df9fa7b687f8d
KingToot14 <armorgamesdot@gmail.com>
2024-06-13 22:08:00 -0400

    Finished Animation Tweaks
    I still need to finish the heavy slam animation

--- shaders/entities/effect_manager.gd
+++ shaders/entities/effect_manager.gd
@@ -4,6 +4,7 @@ extends Node2D
 # --- Variables --- #
 @export var sprite: Sprite2D
 @export var shadow_sprite: Sprite2D
+@export var curr_radius := .12
 
 @export var hit_flash_time: float = 0.1
 
@@ -12,7 +13,13 @@ func set_flash_intensity(intensity: float) -> void:
 	sprite.material.set_shader_parameter('intensity', intensity)
 
 func set_shadow_radius(radius: float) -> void:
+	curr_radius = radius
 	shadow_sprite.material.set_shader_parameter('radius', radius)
+
+func tween_shadow_radius(radius: float, time: float) -> void:
+	var tween = create_tween()
+	
+	tween.tween_method(set_shadow_radius, curr_radius, radius, time)
 
 func _on_lost_health(_dmg: int, _entity: Entity) -> void:
 	do_hit_flash()
