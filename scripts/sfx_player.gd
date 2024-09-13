8a6e8d7280490d7aa3fcf0e839c28897a7cfc57b
KingToot14 <armorgamesdot@gmail.com>
2024-09-13 14:46:24 -0400

    Started sfx

--- scripts/sfx_player.gd
+++ scripts/sfx_player.gd
@@ -2,13 +2,18 @@ class_name SfxPlayer
 extends AudioStreamPlayer
 
 # --- Variables --- #
-var default_volume: float
+
 
 # --- Functions --- #
 func _ready() -> void:
-	default_volume = volume_db
+	if max_polyphony == 1:
+		max_polyphony = 10
+	
+	if not stream or not (stream is AudioStreamPolyphonic):
+		stream = AudioStreamPolyphonic.new()
 
 func play_sfx(sfx: AudioStream, volume_modifier := 0.0) -> void:
-	volume_db = default_volume + volume_modifier
-	stream = sfx
-	play()
+	if !playing: play()
+	
+	var playback = get_stream_playback()
+	playback.play_stream(sfx, 0, volume_modifier)
