7874ea9254d84d8392024a3d634a913f2e482c66
KingToot14 <armorgamesdot@gmail.com>
2024-09-17 15:50:22 -0400

    Finished Falling Objects
    Created a new system for overriding global positions inside an animation

---
+++ scripts/battles/transform_globalizer.gd
@@ -0,0 +1,15 @@
+class_name TransformGlobalizer
+extends Node
+
+# --- Variables --- #
+@export var node_to_push_to: Node2D
+@export var root: Node2D
+
+@export var offset: Vector2
+@export var update_pos := true
+
+# --- Functions --- #
+func _process(_delta: float) -> void:
+	if update_pos:
+		node_to_push_to.global_position = root.global_position + offset
+	
