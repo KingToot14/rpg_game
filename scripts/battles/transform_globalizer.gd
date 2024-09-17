dc72fd246f0fb8890cbc9b8e57c58b8692a134a9
KingToot14 <armorgamesdot@gmail.com>
2024-09-17 16:20:58 -0400

    Added Small Rupture Effects
    These cover the masking from the falling objects and heavy slam attacks

--- scripts/battles/transform_globalizer.gd
+++ scripts/battles/transform_globalizer.gd
@@ -5,11 +5,11 @@ extends Node
 @export var node_to_push_to: Node2D
 @export var root: Node2D
 
-@export var offset: Vector2
+@export var pos_offset: Vector2
 @export var update_pos := true
 
 # --- Functions --- #
 func _process(_delta: float) -> void:
 	if update_pos:
-		node_to_push_to.global_position = root.global_position + offset
+		node_to_push_to.global_position = root.global_position + pos_offset
 	
