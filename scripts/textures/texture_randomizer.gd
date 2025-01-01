69d989b9cb77c0d617d7b1f64edcf9d7de728293
Jacob Vanluven <armorgamesdot@gmail.com>
2025-01-01 17:14:38 -0500

    Implemented New Slime Animations
    Added the new slime animations that I created a long while ago. Updated every existing animation, and finally added the slime fling animation!

--- scripts/textures/texture_randomizer.gd
+++ scripts/textures/texture_randomizer.gd
@@ -2,13 +2,25 @@ class_name TextureRandomizer
 extends Node2D
 
 # --- Variables --- #
-@export var sprite: Node2D
+@export var sprite: Sprite2D
 
+@export var use_frames := true
+# @@show_if(use_frames)
 @export var total_frames: int = 1
+# @@show_if(!use_frames)
+@export var textures: Array[Texture2D] = []
 
 # --- Functions --- #
 func _ready():
 	randomize_texture()
 
 func randomize_texture() -> void:
-	sprite.frame = randi_range(0, total_frames - 1)
+	if use_frames:
+		sprite.frame = randi_range(0, total_frames - 1)
+	else:
+		var texture = textures.pick_random()
+		
+		if texture:
+			sprite.texture = texture
+		else:
+			sprite.hide()
